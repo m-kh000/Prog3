62ab7c6844ef7c05f77419a0db9bb9e95419e712
Joseph <zouzousalloum26@gmail.com>
2025-12-08 18:17:43 -0800

    unfinished

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,5 +1,77 @@
 package utils;
 
-public class FileUtils {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class FileUtils {
+    /**
+     * Reads the data from a provided file.
+     * 
+     * @param file the path to the file
+     * 
+     * @return {@code List<String>} where each element is a line from the file
+     *          or {@code null} if an {@code IOException} occurred.
+     * 
+     * @throws FileNotFoundException if there is no file with the provided path
+     */
+    public static List<String> readFile(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        
+        if (!file.exists())
+            throw new FileNotFoundException("No file with the provided path.");
+
+        List<String> lines = new ArrayList<>();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+
+            return lines;
+        } catch (IOException e) {
+            System.err.println("I/O Error: " + e);
+            return null;
+        }
+    }
+
+    /**
+     * Creates a file and write the specified data on it 
+     * or append the data to an existing file.
+     * <p>
+     *      The writing process is atomic, if any exception happend then
+     *      no data will be added to the file at all.
+     * </p>
+     * 
+     * @param filePath the path to the file
+     * @param data the data to write
+     */
+    public static void saveFile(String filePath, String data) {
+        /* 
+         *                     ------------ IMPORTANT ------------
+         * Atomic File Replacement: For ensuring that a file is either fully updated 
+         * with new content or remains with its old content (without an intermediate state),
+         * a common pattern is to write the new content to a temporary file and then 
+         * atomically rename the temporary file to replace the original. 
+         * This approach is often used for configuration files or data files 
+         * where consistency is crucial.
+         */
+        File file = new File(filePath);
+
+        try {
+            if (file.createNewFile()) {
+                //TODO write data to the file directly
+            } else {
+                //TODO append data to the existing file
+            }
+        } catch (IOException e) {
+
+        }
+    }
 }
--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -1,5 +1,9 @@
 package utils;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 import exceptions.InvalidEmailException;
 
 public class Validator {
@@ -9,7 +13,7 @@ public class Validator {
      * @param email the email to validate
      * @param password the password to check
      * 
-     * @return -1 if the email is not found,
+     * @return -1 if the email is not found or the Users file is not found,
      *         0 if the email is found but the password is incorrect,
      *         1 if both email and password are correct.
      * 
@@ -19,6 +23,13 @@ public class Validator {
     public static int validateEmail(String email, String password) throws InvalidEmailException{
         if (!email.matches("^[a-zA-Z0-9_]+@gmail\\.com$")) {
             throw new InvalidEmailException("Invalid Email Format.");
+        }
+        
+        try {
+            List<String> users = new ArrayList<>(FileUtils.readFile("./files/Users"));
+        } catch (FileNotFoundException e) {
+            System.err.println("Users file not found.");
+            return -1;
         }
         return 0;
     }
