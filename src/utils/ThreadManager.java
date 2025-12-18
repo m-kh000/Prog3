33af5dc41857ae36c38cfe089bc593c721908ab9
Joseph <zouzousalloum26@gmail.com>
2025-12-18 12:55:39 -0800

    add the priority property to the Product class and made the base of the ThreadManager class

--- src/utils/ThreadManager.java
+++ src/utils/ThreadManager.java
@@ -1,6 +1,36 @@
 package utils;
 
+import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
+
+import core.ProductLine;
+
 public class ThreadManager {
-    private Thread[] threadPool = new Thread[5];
-    
+    private static Thread[] threadPool = new Thread[6];
+    private static Queue<ProductLine> waiting = new PriorityBlockingQueue<>(1,
+                             (pl1, pl2) -> {return pl2.getPriority() - pl1.getPriority();});
+
+    /**
+     * 
+     * @param pl
+     */
+    public static void employ(ProductLine pl) {
+        if (pl == null) {
+            return;
+        }
+
+        waiting.add(pl);
+    }
+
+    /**
+     * 
+     */
+    public static void assign() {
+        for (Thread t : threadPool) {
+            if (!t.isAlive()) {
+                t = new Thread(waiting.remove());
+                t.start();
+            }
+        }
+    }
 }
