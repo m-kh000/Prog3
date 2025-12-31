8203ab550380132de9a5d2db5637fe1dd69faaa2
Joseph <zouzousalloum26@gmail.com>
2025-12-30 19:23:21 -0800

    pew pew pew

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -2,8 +2,6 @@ package ui;
 import java.awt.*;
 import javax.swing.*;
 
-import core.Factory;
-
 public class UI extends JFrame {
     private static JPanel centerPanel;
     
@@ -43,9 +41,10 @@ public class UI extends JFrame {
     
 }
 
-//TODO 
-//ids
-//add product
-//json localdate
-//hashset in product
-//deliver a task exception
+// TODO 
+// ids
+// add product
+// json localdate, Done
+// hashset in product
+// deliver a task exception
+// employ and assign new ProductLines automatically
