89eeeb230a3430fd4e6650d681b3db23d3a42adc
MK <marmorakh2000@gmail.com>
2026-01-05 11:11:17 +0300

    .

--- src/ui/functions/ViewPerformance.java
+++ src/ui/functions/ViewPerformance.java
@@ -3,12 +3,23 @@ import core.Factory;
 import java.awt.*;
 import javax.swing.*;
 import ui.components.ProductLinePanel;
+import utils.FileUtils;
 
 public class ViewPerformance extends FunctionPanel {
     public ViewPerformance(JPanel centerPanel, JFrame frame, Factory factory) {
         setLayout(new BorderLayout());
         add(createTopPanel("View Performance", centerPanel, frame, factory, "manager"), BorderLayout.NORTH);
         add(new JScrollPane(createItemsPanel(factory)), BorderLayout.CENTER);
+        while(true){
+            revalidate();
+            repaint();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                FileUtils.log(ex);
+            }
+        }
     }
     
     private JPanel createItemsPanel(Factory factory) {
@@ -16,6 +27,7 @@ public class ViewPerformance extends FunctionPanel {
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
         for (core.ProductLine line : factory.previewLines()) {
             panel.add(new ProductLinePanel(line));
+            panel.add(Box.createVerticalStrut(5));
         }
         if (factory.previewLines().length == 0) {
             panel.add(new JLabel("No lines found."));
